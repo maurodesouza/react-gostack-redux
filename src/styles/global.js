caeb75499512faea864fd8d673ea961e36af3b9e
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-02-22 13:19:50 -0300

    fix font import

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,8 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 import background from '../assets/images/background.svg';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
-
   * {
     padding: 0;
     margin: 0;
