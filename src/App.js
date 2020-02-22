cfd237fdccb7e0c525f403c742ed7ebba9701c9f
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-02-22 18:48:41 -0300

    class 08 - config redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import GlobalStyles from './styles/global';
 
 import Header from './components/Header';
 import Routes from './routes';
 
+import store from './store';
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyles />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyles />
+      </BrowserRouter>
+    </Provider>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { createStore } from 'redux';
+
+import root from './modules/root';
+
+const store = createStore(root);
+
+export default store;
